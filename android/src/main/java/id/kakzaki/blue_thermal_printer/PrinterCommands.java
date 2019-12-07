1850f776416ec1328cf1b5928bddce60f8e65ce7
ElbrusNT <elbrusnt@gmail.com>
2019-12-08 01:40:24 +0400

    Update PrinterCommands.java

--- android/src/main/java/id/kakzaki/blue_thermal_printer/PrinterCommands.java
+++ android/src/main/java/id/kakzaki/blue_thermal_printer/PrinterCommands.java
@@ -19,7 +19,7 @@ public class PrinterCommands {
     public static final byte EOT = 0x04;
 
     public static final byte[] INIT = {27, 64};
-    public static byte[] FEED_LINE = {32};
+    public static byte[] FEED_LINE = {10};
 
     public static byte[] SELECT_FONT_A = {20, 33, 0};
 
